bf4d5a38a6798b0b1f4f2af4a3062be4216f5203
spicynoon <yandiyan10@gmail.com>
2023-05-30 21:06:39 +0700

    on develop

---
+++ lib/model/todo.dart
@@ -0,0 +1,20 @@
+class ToDo {
+  String? id;
+  String? todoText;
+  bool isDone;
+
+  ToDo({
+    required this.id,
+    required this.todoText,
+    this.isDone = false,
+  });
+
+  static List<ToDo> todoList() {
+    return [
+      ToDo (id: '01', todoText: 'Morning Exercise', isDone: true),
+      ToDo (id: '02', todoText: 'Buy Groceries', isDone: true),
+      ToDo (id: '03', todoText: 'Basketball Exercise', ),
+      ToDo (id: '04', todoText: 'Meet People', ),
+    ];
+  }
+}
